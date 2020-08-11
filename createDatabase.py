bdd75214ec5b58fdc94bc7297fbc00933d574de1
Joe Downs <joedowns502@gmail.com>
2020-08-10 22:05:05 -0400

    createDatabase: create file and call init functions
    This file is used to completely create the CDL
    database. As of now, it only is run when the
    database does not exist yet (i.e., the commands
    will fail if the tables are already created)
    
    + Call various functions from initDB

---
+++ createDatabase.py
@@ -0,0 +1,19 @@
+import connectDB
+import initDB
+import readDD
+import writeDB
+
+conn = connectDB.createConnection("cdl.db")
+cursor = connectDB.createCursor(conn)
+
+
+initDB.createEventGamemodesTable(cursor)
+initDB.createEventTable(cursor)
+initDB.createResultsTable(cursor)
+initDB.createRSVPTable(cursor)
+initDB.createSeasonTable(cursor)
+initDB.createUserTable(cursor)
+initDB.createVehicleTable(cursor)
+
+
+

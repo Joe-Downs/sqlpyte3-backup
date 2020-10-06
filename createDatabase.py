90ddcfec58f7b5fc0a8dece23e258657dce39bf6
Joe Downs <joedowns502@gmail.com>
2020-10-05 23:14:38 -0400

    createDatabase.py: add calls to various methods
    + initDB.createReminderTable()
    + connectDB.commitChanges()
    + connectDB.closeConnection()

--- createDatabase.py
+++ createDatabase.py
@@ -9,11 +9,12 @@ cursor = connectDB.createCursor(conn)
 
 initDB.createEventGamemodesTable(cursor)
 initDB.createEventTable(cursor)
+initDB.createReminderTable(cursor)
 initDB.createResultsTable(cursor)
 initDB.createRSVPTable(cursor)
 initDB.createSeasonTable(cursor)
 initDB.createUserTable(cursor)
 initDB.createVehicleTable(cursor)
 
-
-
+connectDB.commitChanges(conn)
+connectDB.closeConnection(conn)

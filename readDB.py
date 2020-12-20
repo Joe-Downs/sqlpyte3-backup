f33d8c956783bd4f6d558fd322b527c29e5389af
Joe Downs <joedowns502@gmail.com>
2020-12-19 23:40:33 -0500

    readDB.py: change getValue() to return a list of multiple values
    By default, the function will only return the first value, but this
    allows it to return multiple data values as well as sort them by a
    certain column in the SQLite table.
    
    ~ getValue()

--- readDB.py
+++ readDB.py
@@ -2,13 +2,23 @@ import sqlite3
 
 # Returns a single data value from a given table,
 # column, and row. Setting getMultiple to True will return a
-# list of all the values which match the search parameters
+# list of all the values which match the search parameters. Setting
+# sortedResults to True will sort the returned data by the values
+# in a given column (ascending, by default)
 # e.g., return the rank for a given name
 # or, return the name for a given rank
 def getValue(cursor, tableName, desiredColumn,
-             searchColumn, searchValue, getMultiple = False):
+             searchColumn, searchValue, getMultiple = False,
+             sortedResults = False, sortBy = "", descending = False):
     selectDataString = "SELECT " + desiredColumn + " FROM " + tableName
     selectDataString += " WHERE " + searchColumn + "=?"
+    if sortedResults == True:
+        # Sort the results by the given colum in the call requests it
+        # The order is ascending by default
+        selectDataString += f"ORDER BY {sortBy} "
+        if descending == True:
+            # Sort the results in descending order
+            selectDataString += f"DESC"
     cursor.execute(selectDataString, (searchValue,))
     # Assigns the list of tuples given by fetchall() [or None] to
     # a variable so it can be accessed multiple times
